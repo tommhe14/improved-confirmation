4c503c3ccfae35bdc31c8996303096959b1bfe1d
mane <53539004+tommhe14@users.noreply.github.com>
2024-06-09 14:12:56 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='discord-confirmation',
+    version='1.0.0',
+    description='A library for creating confirmation messages in Discord interactions',
+    author='mane',
+    author_email='your@email.com',
+    packages=['confirmation'],
+    install_requires=[
+        'discord.py',
+    ],
+)

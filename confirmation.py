8f1ac6e5be80249466cc8323c7b21638565e1b89
mane <53539004+tommhe14@users.noreply.github.com>
2024-06-09 14:12:03 +0100

    Update confirmation.py

--- confirmation.py
+++ confirmation.py
@@ -28,6 +28,7 @@ class Confirmation:
         text: str,
         user: discord.User,
         channel: Optional[discord.TextChannel] = None,
+        hide_author: bool = False,
         timeout: int = 20,
     ) -> Optional[bool]:
         """
@@ -36,11 +37,13 @@ class Confirmation:
         :param text: The confirmation text.
         :param user: The user who has to confirm.
         :param channel: The channel the message will be sent to. Must only be specified if `self.message` is None.
+        :param hide_author: Whether or not the `user` should be set as embed author.
         :param timeout: Seconds to wait until stopping to listen for user interaction.
         :return: True when it's been confirmed, otherwise False. Will return None when a timeout occurs.
         """
         emb = discord.Embed(description=text, color=self.color)
-        emb.set_author(name=str(user), icon_url=user.avatar.url)
+        if not hide_author:
+            emb.set_author(name=str(user), icon_url=user.avatar.url)
 
         self._embed = emb
 
@@ -96,8 +99,10 @@ class BotConfirmation(Confirmation):
         interaction: discord.Interaction,
         color: int = 0x000000,
         message: Optional[discord.Message] = None,
+        ephemeral: bool = False,
     ):
         self._interaction = interaction
+        self.ephemeral = ephemeral
         super().__init__(interaction.client, color, message)
 
     async def confirm(
@@ -105,6 +110,7 @@ class BotConfirmation(Confirmation):
         text: str,
         user: Optional[discord.User] = None,
         channel: Optional[discord.TextChannel] = None,
+        hide_author: bool = False,
         timeout: int = 20,
     ) -> Optional[bool]:
 
@@ -114,4 +120,55 @@ class BotConfirmation(Confirmation):
         if self.message is None and channel is None:
             channel = self._interaction.channel
 
-        return await super().confirm(text, user, channel, timeout)
+        emb = discord.Embed(description=text, color=self.color)
+        if not hide_author:
+            emb.set_author(name=str(user), icon_url=user.avatar.url)
+
+        self._embed = emb
+
+        yes_button = discord.ui.Button(emoji=discord.PartialEmoji(name="✅"), custom_id="confirm_yes")
+        no_button = discord.ui.Button(emoji=discord.PartialEmoji(name="❌"), custom_id="confirm_no")
+
+        view = discord.ui.View()
+        view.add_item(yes_button)
+        view.add_item(no_button)
+
+        await self._interaction.response.send_message(embed=emb, view=view, ephemeral=self.ephemeral)
+        self.message = await self._interaction.original_response()
+
+        def check(interaction):
+            return (
+                interaction.message.id == self.message.id
+                and interaction.user.id == user.id
+                and interaction.data.get("custom_id") in ["confirm_yes", "confirm_no"]
+            )
+
+        try:
+            interaction = await self._client.wait_for("interaction", check=check, timeout=timeout)
+            if interaction.data['custom_id'] == "confirm_yes":
+                self._confirmed = True
+            elif interaction.data['custom_id'] == "confirm_no":
+                self._confirmed = False
+
+            await interaction.response.defer()
+
+        except asyncio.TimeoutError:
+            self._confirmed = None
+
+        finally:
+            await self._interaction.edit_original_response(view=None)
+
+        return self._confirmed
+
+
+    async def update(self, text: str, color: int = 0x000000):
+        """
+        Update the confirmation message.
+
+        :param text: The new text for the confirmation message.
+        :param color: The color of the embed.
+        """
+        if self._embed:
+            self._embed.description = text
+            self._embed.color = color
+            await self._interaction.edit_original_response(embed=self._embed)
